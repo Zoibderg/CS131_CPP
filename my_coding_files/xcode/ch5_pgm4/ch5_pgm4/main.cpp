02014b4147094e941b0ab1b07ce0e0112e921f7b
Austin Tesch <alrtesch@gmail.com>
2024-05-16 23:09:00 -0700

    bulk of code for program 4 -- untested

--- my_coding_files/xcode/ch5_pgm4/ch5_pgm4/main.cpp
+++ my_coding_files/xcode/ch5_pgm4/ch5_pgm4/main.cpp
@@ -19,6 +19,47 @@
 using namespace std;
 
 int main(){
+    // Set floors
+    int lowest_floor = 10, highest_floor = 16;
+    
+    int const TOTAL_SUITES = 120;
+    int occupied_suites = 0;
+    
+    // Iterate thorugh floors
+    int current_floor = lowest_floor;
+    
+    while (current_floor <= highest_floor)
+    {
+        if (current_floor == 13){
+            continue;
+        }
+        
+        // Promt user for number off occupied suites on current floor
+        cout << "How many suites are occupied on floor " << current_floor << "?" << endl;
+        
+        int user_input;
+        cin >> user_input;      // Store input for validation
+        
+        // Validate input
+        while (user_input < 0 || user_input > 20){
+            cout << "Invalid input."
+            << "How many suites are occupied on floor " << current_floor << "?" << endl;
+            
+            cin >> user_input;
+        }
+        
+        occupied_suites += user_input;
+    }
+    
+    // Display total number of suites avaliable
+    int suites_avaliable = TOTAL_SUITES - occupied_suites;
+    
+    cout << "The top 5 floors of the hotel has " << suites_avaliable << " avaliable suites." << endl;
+    
+    // Display occupied suites
+    cout << occupied_suites << " of them are occupied." << endl;
+    
+    // Display percentage of occupied suites
     
     return 0;
 }
