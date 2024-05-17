5a6280fdac4a99981c94dfc659d9f5fa37bfd487
Austin Tesch <alrtesch@gmail.com>
2024-05-17 15:10:20 -0700

    create template for midterm, wish me luck

---
+++ my_coding_files/xcode/cs131_midterm/cs131_midterm/main.cpp
@@ -0,0 +1,18 @@
+/*
+ 
+ Programmer: Austin Tesch
+ Date modified: 00-00-00
+ Compiler used:  XCODE v. 15.0
+ 
+ Purpose:
+
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
