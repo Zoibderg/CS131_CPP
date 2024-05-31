62427dfb4dcd4b9ff9e04f0c1f64f3a25f526362
Austin Tesch <alrtesch@gmail.com>
2024-05-31 00:25:14 -0700

    create files for ch7_pgm2

---
+++ my_coding_files/xcode/ch7_pgm2/ch7_pgm2/main.cpp
@@ -0,0 +1,27 @@
+/*
+ 
+ Programmer: Austin Tesch
+ Date modified: 05-31-24
+ Compiler used:  XCODE v. 15.0
+ 
+ Purpose:
+ A program that uses a structure named CorpData to store the following information on a company division: 
+ division name (e.g., East, West, North, or South), qtr1 sales, qtr2 sales, qtr3 sales, and qtr4 sales
+
+ Include a constructor that allows the division name and four quarterly sales amount to be specified at the time a CorpData variable is created.
+
+ The program should create four CorpData variables, each representing one of the following corporate divisions: 
+ East, West, North, and South.
+ 
+ These variables should be passed one at a time, as constant references, to a function that computes the division’s annual sales total and quarterly average, and displays these along with the division name.
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
