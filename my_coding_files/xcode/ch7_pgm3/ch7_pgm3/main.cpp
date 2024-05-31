b2b7c6314badad2a1d6a33a23c4cd8c431f36c65
Austin Tesch <alrtesch@gmail.com>
2024-05-31 01:00:26 -0700

    create ch7_pmg3

---
+++ my_coding_files/xcode/ch7_pgm3/ch7_pgm3/main.cpp
@@ -0,0 +1,27 @@
+/*
+ 
+ Programmer: Austin Tesch
+ Date modified: 05-31-24
+ Compiler used:  XCODE v. 15.0
+ 
+ Purpose:
+ Youth soccer teams earn 3 points for each win, 1 point for each tie, and 0 points for each loss.
+ 
+ Create a teamScore class with variables to hold the teamName and count the number of wins, ties, and losses.
+ 
+ The constructor should accept the team name and initialize the three counters to 0.
+ 
+ The class should have member functions updateWins, updateTies, and updateLosses that each add 1 to the appropriate counter. It should also have a displayRecord function that produces a nicely formatted display containing the team name, the number of wins, ties, and losses so far, and the computed points.
+ 
+ Demonstrate the class by creating a menu-driven program that creates a teamScore object and then includes a loop to display a menu and call the appropriate class function depending on the user-entered menu choice.
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
