f09a53ef1741df702d1a2597ae77e89671379585
Austin Tesch <alrtesch@gmail.com>
2024-05-31 10:37:19 -0700

    pgm4 complete

--- my_coding_files/xcode/ch7_pmg4/ch7_pmg4/main.cpp
+++ my_coding_files/xcode/ch7_pmg4/ch7_pmg4/main.cpp
@@ -24,10 +24,67 @@
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
-int main(){
-    
+class Coin {
+private:
+    string sideUp;
+
+public:
+    // Default constructor that randomly sets the sideUp variable
+    Coin() {
+        toss();
+    }
+
+    // simulate tossing the coin
+    void toss() {
+        int randomValue = rand() % 2; // Generate 0 or 1
+        if (randomValue == 0) {
+            sideUp = "heads";
+        } else {
+            sideUp = "tails";
+        }
+    }
+
+    // return the value of sideUp
+    string getSideUp() {
+        return sideUp;
+    }
+};
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
+
+    Coin myCoin;
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    // Display the initial side facing up
+    cout << "Initial side up: " << myCoin.getSideUp() << endl;
+
+    // Toss the coin 20 times
+    for (int i = 0; i < 20; ++i) {
+        myCoin.toss();
+        string result = myCoin.getSideUp();
+        cout << "Toss " << i + 1 << ": " << result << endl;
+
+        // Update the counts
+        if (result == "heads") {
+            ++headsCount;
+        } else {
+            ++tailsCount;
+        }
+    }
+
+    // Display the final counts
+    cout << "\n ----------------------------- \n" << endl;
+    cout << "Final counts: " << endl;
+    cout << "Heads count: " << headsCount << endl;
+    cout << "Tails count: " << tailsCount << endl;
+
     return 0;
 }
