32509faf18219d7e87d5d935a641afac47c85856
Austin Tesch <alrtesch@gmail.com>
2024-05-30 22:37:23 -0700

    create ch7_pgm1 main

---
+++ my_coding_files/xcode/ch7_pmg1/ch7_pmg1/main.cpp
@@ -0,0 +1,27 @@
+/*
+ 
+ Programmer: Austin Tesch
+ Date modified: 05-30-24
+ Compiler used:  XCODE v. 15.0
+ 
+ Purpose:
+ Write a program that asks the user for the number of widgets that have been ordered and then displays the number of days it will take to produce them.
+ 
+ --------------------------------------------------------------------------------------------
+ 
+ Think about what values your program should accept for the number of widgets ordered.
+ 
+ Design a class for a widget manufacturing plant. 
+ 
+ Assuming that 10 widgets may be produced each hour, the class object will calculate how many days it will take to produce any number of widgets. (The plant operates two 8-hour shifts per day.)
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
