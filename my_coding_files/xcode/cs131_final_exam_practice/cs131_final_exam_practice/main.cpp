b9f5767745ed8cd19cf2976153d8678f9cdd9c23
Austin Tesch <alrtesch@gmail.com>
2024-06-11 09:27:48 -0700

    create template for final exam question practice

---
+++ my_coding_files/xcode/cs131_final_exam_practice/cs131_final_exam_practice/main.cpp
@@ -0,0 +1,51 @@
+/*
+ Programmer: Austin Tesch
+ Date modified: 6/11/24
+ Compiler used:  XCODE v. 15.0
+
+cs131_final_exam_practice
+ 
+ Purpose:
+ 
+ Part 1:
+ 
+ Write a class, SalesCls, that contains two attributes (salesAmount and SalesRep). The class must have a constructor and a member function(s). The class must do desired tasks: Assign values (any sales amount, any sales representative's name), and calculate a commission amount based on the conditions below.
+ 
+ Commission Rate Table:
+
+ Sales Amount                   Commission Rate
+
+ < 5000                                      1%
+
+ between 5000 and 10000         2%
+
+ > 10000                                    3%
+ 
+ The main( ) must declare an object of the class. Use the object to call a class constructor and other member functions. The output should display the name, sales amount, and commission earned. Inputs could come from the user's inputs in the main( ).
+
+ Write the class and the main( ) that has one object declared. The object calls the appropriate member function(s).
+ 
+ ----------------------------------
+ 
+ Part 2:
+
+ Modify the program in which the sales amount, sales representative's name, and calculated commission amount will be written to disk. Review Chapter 5 Pages 287-292. Examine programs pr5_16.cpp and pr5_17.cpp.
+ 
+ ------------------------------------
+ 
+ Part 3:
+ 
+ --------------------------------------
+
+    Insert two lines of code toward the end of the program.  The first line is to create an array of three objects in the main ( ).  Review Chapter 8 Section 8.12. Program 8_28.cpp is a good example.  The second line of code is to use the object at subscript/index zero, to call a member function.
+
+*/
+
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
