f800aafbcfbc4e5b00fe381c9c81e91a5496fb20
Austin Tesch <alrtesch@gmail.com>
2024-06-12 12:55:06 -0700

    create template for final exam

---
+++ my_coding_files/xcode/cs131_final_exam/cs131_final_exam/main.cpp
@@ -0,0 +1,20 @@
+/*
+ Programmer: Austin Tesch
+ Date modified: 6/12/24
+ Compiler used:  XCODE v. 15.0
+
+cs131_final_exam
+ 
+ Purpose:
+
+*/
+
+#include <iostream>
+
+// only used to simplify code for this class
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
